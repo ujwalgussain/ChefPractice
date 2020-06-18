3473429f46bdb10113ba803701628d91e9c31d01
Ujwal Gussain <ujwalgussain@gmail.com>
2020-06-18 09:34:07 +0530

    changes

--- src/MultithreadingSample/ForkJoinPoolSample.java
+++ src/MultithreadingSample/ForkJoinPoolSample.java
@@ -2,7 +2,10 @@ package MultithreadingSample;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-
+/*
+* ForkJoinPool accepts 2 kind of tasks classes:
+*   RecursiveAction : Which doesn't return any value
+*   RecursiveTask : Which Returns a value*/
 class MyRecursiveTask extends RecursiveTask<Integer>
 {
     int no;
@@ -12,19 +15,24 @@ class MyRecursiveTask extends RecursiveTask<Integer>
     }
     @Override
     protected Integer compute() {
+        System.out.println("Thread started "+ Thread.currentThread().getName());
         if(no==0)
         {
+            System.out.println("Thread started "+ Thread.currentThread().getName() + " returned 0");
             return 0;
         }
         if(no==1)
         {
+            System.out.println("Thread "+ Thread.currentThread().getName() + " returned 1");
             return 1;
         }
         MyRecursiveTask t1 = new MyRecursiveTask(no-1);
         t1.fork();
         MyRecursiveTask t2 = new MyRecursiveTask(no-2);
         t2.fork();
-        return t1.join()+t2.join();
+        int res =  t1.join()+t2.join();
+        System.out.println("Thread "+ Thread.currentThread().getName() + " returned "+res);
+        return res;
     }
 }
 public class ForkJoinPoolSample {
