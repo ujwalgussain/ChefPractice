8bfa25dddfca50d11a181b99037420e3b4f3f0a3
Gussain <ugussain@deloitte.com>
2021-06-14 12:56:38 +0530

    changes

---
+++ src/Java_Basics/InnerClassTest.java
@@ -0,0 +1,52 @@
+package Java_Basics;
+
+
+import java.util.HashMap;
+
+class Outer {
+    private int a;
+
+    public void outerMethod()
+    {
+        System.out.println("outer method");
+    }
+    class NonStaticInner {
+        int b;
+        public void m1()
+        {
+            System.out.println(a);
+            System.out.println(b);
+            outerMethod();
+        }
+    }
+
+    static class StaticInner {
+        int c;
+        public void m2()
+        {
+            /*System.out.println(a);
+            System.out.println(b);
+            outerMethod();*/
+        }
+    }
+    Outer()
+    {
+        NonStaticInner non_staticInner = this. new NonStaticInner();// association with outer Object
+        StaticInner staticInner = new StaticInner();
+    }
+}
+
+public class InnerClassTest {
+
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        Outer.NonStaticInner nonStaticInner = new Outer().new NonStaticInner();
+
+        Outer.StaticInner staticInner = new Outer.StaticInner(); // No reference to outer class
+
+    }
+
+
+}
