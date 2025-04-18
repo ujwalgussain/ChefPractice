81ea843141c4f6b82c5280d703e74e2c9a1b9160
ujwal <ujwalgussain@gmail.com>
2025-04-18 10:40:54 +0530

    knapsack dp update

--- src/Dynamic_Programming/KnapSackProblem/KnapsackDPImpl.java
+++ src/Dynamic_Programming/KnapSackProblem/KnapsackDPImpl.java
@@ -1,7 +1,6 @@
 package Dynamic_Programming.KnapSackProblem;
 
 
-import java.util.Set;
 
 public class KnapsackDPImpl {
     //GFG: https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
@@ -20,6 +19,36 @@ public class KnapsackDPImpl {
             profitWithCurr = cost[n - 1] + knapSack(W - wt[n - 1], wt, cost, n - 1);
             return Math.max(profitWithCurr,profitWithoutCurr);
         }
+    }
+
+    // Returns the maximum value that
+    // can be put in a knapsack of capacity W
+    static int knapsack(int W, int[] val, int[] wt) {
+        int n = wt.length;
+        int[][] dp = new int[n + 1][W + 1];
+
+        // Build table dp[][] in bottom-up manner
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+
+                // If there is no item or the knapsack's capacity is 0
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else {
+                    int pick = 0;
+
+                    // Pick ith item if it does not exceed the capacity of knapsack
+                    if (wt[i - 1] <= j)
+                        pick = val[i - 1] + dp[i - 1][j - wt[i - 1]];
+
+                    // Don't pick the ith item
+                    int notPick = dp[i - 1][j];
+
+                    dp[i][j] = Math.max(pick, notPick);
+                }
+            }
+        }
+        return dp[n][W];
     }
 
     public static void main(String args[]) {
